b3dab0de87553060c72e05c591b3413ef4678a2d
muhammad-abdullah11 <abdullahworld111@gmail.com>
2026-04-15 17:05:08 +0500

    refctor:change the home icon color gray-900 from blue-600

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import SearchBar from '../components/SearchBar'
 export default function Home() {
   return (
     <main className="flex flex-1 flex-col items-center justify-center gap-8 px-6 py-20 bg-white">
-      <FaGithub className="text-8xl text-blue-600" />
+      <FaGithub className="text-8xl text-gray-900" />
       <div className="flex flex-col items-center gap-3 text-center">
         <h1 className="text-4xl sm:text-5xl font-bold tracking-tight text-black">GitHub Profile Visualizer</h1>
         <p className="max-w-md text-lg text-gray-500">
